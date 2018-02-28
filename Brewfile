8dc1b5dfd0da0eeaa6c9033573f629bc1cad18f3
Jeongseok Lee <jslee02@gmail.com>
2018-02-27 18:43:18 -0800

    Add Brewfile

---
+++ Brewfile
@@ -0,0 +1,15 @@
+tap 'dartsim/dart' # for ipopt
+
+brew 'assimp'
+brew 'boost'
+brew 'bullet'
+brew 'eigen'
+brew 'fcl'
+brew 'flann'
+brew 'ipopt'
+brew 'libccd'
+brew 'nlopt'
+brew 'ode', args: ['--with-libccd', '--with-double-precision']
+brew 'open-scene-graph'
+brew 'tinyxml2'
+brew 'urdfdom'
--- ci/install_osx.sh
+++ ci/install_osx.sh
@@ -2,20 +2,5 @@
 
 set -e
 
-brew tap dartsim/dart # for ipopt
-
 brew update > /dev/null
-
-brew list assimp &>/dev/null || brew install assimp
-brew list boost &>/dev/null || brew install boost
-brew list bullet &>/dev/null || brew install bullet
-brew list eigen &>/dev/null || brew install eigen
-brew list fcl &>/dev/null || brew install fcl
-brew list flann &>/dev/null || brew install flann
-brew list libccd &>/dev/null || brew install libccd
-brew list libipoptccd &>/dev/null || brew install ipopt
-brew list nlopt &>/dev/null || brew install nlopt
-brew list ode &>/dev/null || brew install ode
-brew list open-scene-graph &>/dev/null || brew install open-scene-graph
-brew list tinyxml2 &>/dev/null || brew install tinyxml2
-brew list urdfdom &>/dev/null || brew install urdfdom
+brew bundle
