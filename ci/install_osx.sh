6b5f04cf8c7024473454c93ccb022512b195b890
Jeongseok Lee <jslee02@gmail.com>
2018-02-27 19:38:44 -0800

    Skip nlopt installation

--- ci/install_osx.sh
+++ ci/install_osx.sh
@@ -14,7 +14,7 @@ brew list fcl &>/dev/null || brew install fcl
 #brew list flann &>/dev/null || brew install flann
 #brew list ipopt &>/dev/null || brew install ipopt
 brew list libccd &>/dev/null || brew install libccd
-brew list nlopt &>/dev/null || brew install nlopt
+#brew list nlopt &>/dev/null || brew install nlopt
 brew list ode &>/dev/null || brew install ode
 brew list open-scene-graph &>/dev/null || brew install open-scene-graph
 brew list tinyxml2 &>/dev/null || brew install tinyxml2
